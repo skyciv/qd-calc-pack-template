dcfc30a0c8b41163633e1fd6291e636b39412bcd
Sam Carigliano <sam@skyciv.com>
2023-02-20 15:15:04 +1100

    latest version

--- README.md
+++ README.md
@@ -22,5 +22,7 @@ File | Required | Description
 `calculate.js` | TRUE | This is where your calculation code goes. Program your design checks based off a few inputs (defined by the config.json) 
 `config.json `| TRUE |This is the schema of your inputs, this will be read by the UI to generate the input form
 `s3d_integration.js` | FALSE | If you want this calc pack to be available as an integrated module in S3D, you will need to write a function that compiles an array of input jsons
+`ui.js` | FALSE | Want to run some custom code in the UI when your input changes (for instance changing some live graphics)
 `test_files` | TRUE | These must be added if you want your calculations to be available in the platform! This is a QA process and makes testing a lot easier
+
 
--- runTest.js
+++ runTest.js
@@ -1,11 +1,47 @@
-const https = require('https');
+//const https = require('https');
 const fs = require('fs');
-const test_file_name = "test_file_001";
-	
-var config_file = require(__dirname + "/config.json");
-var calculate_js = require(__dirname + "/calculate.js");
-var input = require(__dirname + "/"+test_file_name+'input.json');
+const request = require('request');
 
+
+const test_file_name = "verified_001";
+
+
+var config_file = fs.readFileSync(__dirname + "/config.json", 'utf8');
+var calculate_js = fs.readFileSync(__dirname + "/calculate.js", 'utf8');
+var input = fs.readFileSync(__dirname + "/test_files/"+test_file_name+'/input.json', 'utf8');
+var s3d_model;
+try {
+  s3d_model = fs.readFileSync(__dirname + "/test_files/"+test_file_name+'/s3d_model.json', 'utf8');
+} catch(e) {
+  
+}
+
+var url = "https://dev.skyciv.com:8088/runTestScript";
+//url = "https://skyciv.test:8087/runTestScript";
+var postData = {
+  config_file: config_file,
+  calculate_js: calculate_js,
+  input: input
+};
+
+
+request.post(url, {json: true, body: postData}, function(err, res, body) {
+  console.log(err)
+    if (!err && res.statusCode === 200) {
+        funcTwo(body, function(err, output) {
+            console.log(err, output);
+        });
+    }
+});
+
+
+function funcTwo(input, callback) {
+    // process input
+    console.log(input)
+    callback(null, input);
+}
+
+/*
 var postData = JSON.stringify({
     config_file: config_file,
     calculate_js: calculate_js,
@@ -15,7 +51,7 @@ var postData = JSON.stringify({
 var options = {
   hostname: 'https://dev.skyciv.com',
   port: 8088,
-  path: '/run',
+  path: '/runTestScript',
   method: 'POST',
   headers: {
        'Content-Type': 'application/x-www-form-urlencoded',
@@ -28,13 +64,14 @@ var req = https.request(options, (res) => {
   console.log('headers:', res.headers);
 
   res.on('data', (d) => {
-    process.stdout.write(d);
+    process.stdout.write("data",d);
   });
 });
 
 req.on('error', (e) => {
-  console.error(e);
+  console.error("error",e);
 });
 
 req.write(postData);
 req.end();
+*/
